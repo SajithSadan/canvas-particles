236a04e41080d9e9b2ccbd52e350ace1cb0a2e91
Sajith Sadanandan <sajith.developer@gmail.com>
2021-01-07 22:59:49 +0530

    Partcles and animation added
    - Created particles
    - Animation added to particle by giving weight and wind properties
    - Particle color set to random

--- src/js/game.js
+++ src/js/game.js
@@ -1,1 +1,29 @@
-import '../css/app.css';
+import Particle from './particle'
+
+export default class Game {
+    constructor(canvasId, elmId) {
+        this.element = document.getElementById(elmId);
+        this.canvas = document.getElementById(canvasId);
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
+        this.ctx = this.canvas.getContext("2d");
+        this.particles = [];
+        this.maxParticles = 100;
+    }
+    init() {
+        for(let i=0; i<this.maxParticles; i++){
+            this.particles.push(new Particle(Math.random()*this.canvas.width, 0, 20, this.ctx))
+        }
+        this.animate()
+    }
+    animate() {
+        // clear canvas frame for fading effect
+        this.ctx.fillStyle = 'rgba(0,0,0,0.05)'
+        this.ctx.fillRect(0,0,this.canvas.width, this.canvas.height)
+        this.particles.map(particle => {
+            particle.update()
+            particle.draw()
+        })
+        requestAnimationFrame(()=>this.animate())
+    }
+}
---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+import '../css/app.css'
+import Game from './game'
+
+document.addEventListener('DOMContentLoaded', () => {
+    const game = new Game("canvas", "title")
+    game.init()
+})
