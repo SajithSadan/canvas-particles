3cf40492bf088090e5d9953b481e5b917e40f68d
Sajith Sadanandan <sajith.developer@gmail.com>
2021-01-07 23:52:37 +0530

    Handle window resize

--- src/js/game.js
+++ src/js/game.js
@@ -9,15 +9,32 @@ export default class Game {
         this.ctx = this.canvas.getContext("2d");
         this.particles = [];
         this.maxParticles = 100;
+        this.gameTime = 0;
     }
-    init() {
-        for(let i=0; i<this.maxParticles; i++){
-            this.particles.push(new Particle(Math.random()*this.canvas.width, 0, 20, this.ctx))
+    begin() {
+        // find obstacle
+        const obstacle = this.element.getBoundingClientRect();
+        this.obstacle = {
+            x: obstacle.left,
+            y: obstacle.top,
+            width: obstacle.width,
+            height: obstacle.height,
         }
-        this.animate()
+
+        // rest particles array
+        this.particles = [];
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
+
+        // create new particles and do animate
+        for(let i=0; i<this.maxParticles; i++){
+            this.particles.push(new Particle(Math.random()*this.canvas.width, Math.random() * 1 -100, 20, this.ctx))
+        }
     }
     animate() {
+        this.gameTime++;
         // clear canvas frame for fading effect
+        // this.ctx.clearRect(0,0,this.canvas.width, this.canvas.height)
         this.ctx.fillStyle = 'rgba(0,0,0,0.05)'
         this.ctx.fillRect(0,0,this.canvas.width, this.canvas.height)
         this.particles.map(particle => {
@@ -25,5 +42,10 @@ export default class Game {
             particle.draw()
         })
         requestAnimationFrame(()=>this.animate())
+    }
+    init() {
+        this.begin();
+        window.addEventListener('resize', () => this.begin());
+        this.animate()
     }
 }
